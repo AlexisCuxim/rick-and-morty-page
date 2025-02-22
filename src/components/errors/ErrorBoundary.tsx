7c576d06b54fee13c90d882f5106ee36e7edf6f2
AlexisCuxim <acuximpoot@gmail.com>
2025-02-22 12:39:30 -0500

    error component

---
+++ src/components/errors/ErrorBoundary.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+
+class ErrorBoundary extends React.Component<any, { hasError: boolean }>{
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    return { hasError: true };
+  }
+
+  render(): React.ReactNode {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
